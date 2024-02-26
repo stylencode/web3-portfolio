3a3059f0c7b95ec29748f81e0764dde17dbe18f4
Fuad Talybov <fuad.talybov@gmail.com>
2024-02-26 11:23:17 -0700

    initial commit

---
+++ src/data/profile.ts
@@ -0,0 +1,67 @@
+export const profile = {
+  name: "Sero",
+  role: "Software Engineer",
+  bio: "I'm a software engineer that loves building products and teaching others how to do the same.",
+  imageUrl: "https://i.imgur.com/2m4XIqN.png",
+  address: "0xc0163E58648b247c143023CFB26C2BAA42C9d9A9",
+  links: [
+    {
+      name: "Twitter",
+      url: "https://twitter.com/seroxdesigns",
+      icon: "twitter",
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/Seroxdesign",
+      icon: "",
+    },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/sero-346b85202/",
+      icon: "",
+    },
+    {
+      name: "Blog",
+      url: "https://blog.serotonindesigns.com",
+      icon: "",
+    },
+    {
+      name: 'Podcast',
+      url: 'https://open.spotify.com/show/2HfqI2dFN85uLLFwGqEk0d?si=f918a9d9ea6f4d38',
+      icon: ''
+    }
+  ],
+  projects: [
+    {
+      name: 'ETH DENVER BUIDL REPO',
+      description: 'My work at the ETH Denver hackathon',
+      imageUrl: '',
+      url: 'https://github.com/Seroxdesign/buidl-portfolio'
+    },
+    {
+      name: 'Superfluid',
+      description: '',
+      imageUrl: '',
+      url: 'https://console.superfluid.finance'
+    },
+    {
+      name: 'Tellor',
+      description: '',
+      imageUrl: '',
+      url: 'https://tellor.io'
+    },
+    {
+      name: 'Pokt',
+      description: '',
+      imageUrl: '',
+      url: 'https://pokt.network'
+    },
+    {
+      name: 'Gitcoin',
+      description: '',
+      imageUrl: '',
+      url: 'https://gitcoin.co'
+    }
+
+  ]
+}
