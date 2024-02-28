131b15944843841e907078e1058fbe01c5a7a452
Fuad Talybov <fuad.talybov@gmail.com>
2024-02-27 20:19:01 -0700

    update profile.ts

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,5 +1,5 @@
 export const profile = {
-  name: "Sero",
+  name: "Fuad",
   role: "Software Engineer",
   bio: "I'm a software engineer that loves building products and teaching others how to do the same.",
   imageUrl: "https://i.imgur.com/2m4XIqN.png",
@@ -19,16 +19,6 @@ export const profile = {
       name: "LinkedIn",
       url: "https://www.linkedin.com/in/sero-346b85202/",
       icon: "",
-    },
-    {
-      name: "Blog",
-      url: "https://blog.serotonindesigns.com",
-      icon: "",
-    },
-    {
-      name: 'Podcast',
-      url: 'https://open.spotify.com/show/2HfqI2dFN85uLLFwGqEk0d?si=f918a9d9ea6f4d38',
-      icon: ''
     }
   ],
   projects: [
@@ -37,30 +27,6 @@ export const profile = {
       description: 'My work at the ETH Denver hackathon',
       imageUrl: '',
       url: 'https://github.com/Seroxdesign/buidl-portfolio'
-    },
-    {
-      name: 'Superfluid',
-      description: '',
-      imageUrl: '',
-      url: 'https://console.superfluid.finance'
-    },
-    {
-      name: 'Tellor',
-      description: '',
-      imageUrl: '',
-      url: 'https://tellor.io'
-    },
-    {
-      name: 'Pokt',
-      description: '',
-      imageUrl: '',
-      url: 'https://pokt.network'
-    },
-    {
-      name: 'Gitcoin',
-      description: '',
-      imageUrl: '',
-      url: 'https://gitcoin.co'
     }
 
   ]
